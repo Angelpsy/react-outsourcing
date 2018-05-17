d747326e109b548eb46f6b36c6992bf26c5a8dd6
A.Alekseev <angelpsymail@gmail.com>
2018-05-18 02:57:49 +0500

    add HMR, remove lorem code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById('root');
+const render = () => {ReactDOM.render(<App />, rootEl)};
+
+render();
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        render();
+    })
+}
